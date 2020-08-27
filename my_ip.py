82ab0a5537a436a2ad4c02c377c26035d6defd34
Tom Propst <tom.propst@gmail.com>
2020-08-27 13:57:30 -0600

    local_ip

--- my_ip.py
+++ my_ip.py
@@ -1,10 +1,33 @@
 import urllib.request
+import urllib.error
 import re
+import socket
+
+
+def valid_ipv4(ip_string):
+    pattern = re.compile(r"^(?:\d{1,3}\.){3}\d{1,3}$")
+    return re.match(pattern, ip_string)
+
+
+def valid_ip(ip_string):
+    return valid_ipv4(ip_string)
 
 
 def public_ip(url="http://ipinfo.io/ip"):
-    resource = urllib.request.urlopen(url)
+    try:
+        resource = urllib.request.urlopen(url)
+    except urllib.error.URLError as e:
+        return None
     content = resource.read().decode(resource.headers.get_content_charset())
     ip_string = content.strip()
-    pattern = re.compile(r"(?:\d{1,3}\.){3}\d{1,3}")
-    return ip_string if re.match(pattern, ip_string) else None
+    return ip_string if valid_ipv4(ip_string) else None
+
+
+def local_ip(remote_ip="8.8.8.8", remote_port=80):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            s.connect((remote_ip, remote_port))
+            ip_string = s.getsockname()[0]
+            return ip_string if valid_ipv4(ip_string) else None
+        except OSError as e:  # Most likely, network unreachable
+            return None
--- tests/test_my_ip.py
+++ tests/test_my_ip.py
@@ -5,6 +5,22 @@ import my_ip
 
 class TestMyIP(unittest.TestCase):
 
+    def test_valid_ipv4_good(self):
+        self.assertTrue(my_ip.valid_ipv4("127.0.0.1"))
+        self.assertTrue(my_ip.valid_ipv4("255.255.255.255"))
+        self.assertTrue(my_ip.valid_ipv4("0.0.0.0"))
+        self.assertTrue(my_ip.valid_ipv4("1.1.1.1\n"))
+
+    def test_valid_ipv4_bad(self):
+        self.assertFalse(my_ip.valid_ipv4("hello"))
+        self.assertFalse(my_ip.valid_ipv4("1.1.1.a"))
+        self.assertFalse(my_ip.valid_ipv4("2241.4.1.9"))
+        self.assertFalse(my_ip.valid_ipv4("224,4.1.9"))
+
+    def test_valid_ip(self):
+        self.test_valid_ipv4_good()
+        self.test_valid_ipv4_bad()
+
     def test_public_ip_default_site(self):
         public_ip = my_ip.public_ip()
         self.assertIsNotNone(public_ip)
@@ -12,3 +28,7 @@ class TestMyIP(unittest.TestCase):
     def test_public_ip_bad_response(self):
         response = my_ip.public_ip("https://google.com")
         self.assertIsNone(response)
+
+    def test_local_ip_default(self):
+        local_ip = my_ip.local_ip()
+        self.assertIsNotNone(local_ip)
